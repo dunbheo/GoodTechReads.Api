12337ddfc836be2c77131f118ac9ca9a16b4bb2b
Kyle Hoff <kylehoff01@gmail.com>
2019-12-05 20:47:24 -0700

    Added BookController with WebApis.shared project for endpoint models. Updated launchUrl

---
+++ GoodTechReads.Api.WebApi.Shared/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GoodTechReads.Api.WebApi.Shared.Models
+{
+  public class Book
+  {
+    public int BookId { get; set; }
+    public string Title { get; set; }
+    public DateTime PublicationDate { get; set; }
+    public Author[] Authors { get; set; }
+  }
+}
---
+++ GoodTechReads.Api.WebApi/Controllers/BookController.cs
@@ -0,0 +1,41 @@
+﻿using GoodTechReads.Api.WebApi.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace GoodTechReads.Api.WebApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class BookController : ControllerBase
+  {
+    [HttpGet]
+    public IActionResult Get()
+    {
+      var books = new Book[]
+      {
+        new Book
+        {
+          BookId = 1,
+          Title = "Book 1",
+          PublicationDate = DateTime.Now,
+          Authors = Array.Empty<Author>()
+        },
+        new Book
+        {
+          BookId = 1,
+          Title = "Book 1",
+          PublicationDate = DateTime.Now,
+          Authors = Array.Empty<Author>()
+        },
+        new Book
+        {
+          BookId = 1,
+          Title = "Book 1",
+          PublicationDate = DateTime.Now,
+          Authors = Array.Empty<Author>()
+        }
+      };
+      return Ok(books);
+    }
+  }
+}
